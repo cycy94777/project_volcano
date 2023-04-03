1669d5248876f6ef522f9a66547f0fb36775b595
楊思婷 <yangsiting@christines-MacBook-Air.local>
2023-04-02 20:43:43 -0700

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 d3.json('http://127.0.0.1:5000/data').then(function(data){
     console.log(data[0]);
     var myMap=L.map("map", {
-        center: [45, 30],
+        center: [30, 30],
         zoom: 2,
     })
     var streetMap=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
@@ -10,9 +10,9 @@ d3.json('http://127.0.0.1:5000/data').then(function(data){
     for (let i=0; i<data.length; i++){
         
         let lon=data[i]['lon'];
-        console.log();
+        //console.log();
         let lat=data[i]['lat'];
         
-        L.marker([lon,lat]).addTo(myMap);
+        L.marker([lat,lon]).addTo(myMap);
     };
 });
